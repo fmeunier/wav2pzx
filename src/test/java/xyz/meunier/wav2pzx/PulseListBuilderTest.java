faf04150eff3265f1165a5a9bb024f04a611e25c
Fredrick Meunier <fredm@spamcop.net>
2016-05-09 21:53:09 +1000

    Refactor unit tests.

--- src/test/java/xyz/meunier/wav2pzx/PulseListBuilderTest.java
+++ src/test/java/xyz/meunier/wav2pzx/PulseListBuilderTest.java
@@ -1,30 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Copyright (c) 2016, Fredrick Meunier
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 package xyz.meunier.wav2pzx;
 
-import java.util.List;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.assertThat;
 
 /**
  *
- * @author fred
+ * @author Fredrick Meunier
  */
 public class PulseListBuilderTest {
     
     private static final double TOLERANCE = 0.001;
 	// Two 238 t state pulses with a trailing 79 t state pulse
-    public int[] samples1 = {0, 0, 0, 255, 255, 255, 0};
-    public int[] samples2 = {255, 255, 255, 0, 0, 0, 0, 255};
-    public static final float SAMPLE_RATE = (float) 44100.0;
-    public static final float MACHINE_HZ = (float) 3500000.0;
-    public PulseListBuilder instance1;
-    public PulseListBuilder instance2;
+    private int[] samples1 = {0, 0, 0, 255, 255, 255, 0};
+    private int[] samples2 = {255, 255, 255, 0, 0, 0, 0, 255};
+    private static final float SAMPLE_RATE = (float) 44100.0;
+    private static final float MACHINE_HZ = (float) 3500000.0;
+    private PulseListBuilder instance1;
+    private PulseListBuilder instance2;
     
     @Before
     public void setUp() {
@@ -49,46 +74,42 @@ public class PulseListBuilderTest {
     /**
      * Test of addSample method, of class PulseListBuilder.
      */
-    @Test
-    public void testAddSample() {
-        System.out.println("addSample");
-        int sample = 0;
-        boolean gotException = false;
-        
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddSampleThrowsExceptionForNegative() {
         // Test 1: shouldn't be able to add out of range samples
-        try {
-            instance1.addSample(-1);
-        } catch (IllegalArgumentException e) {
-            gotException = true;
-        }
-        assertTrue(gotException);
-        
-        gotException = false;
-        try {
-            instance1.addSample(256);
-        } catch (IllegalArgumentException e) {
-            gotException = true;
-        }
-        assertTrue(gotException);
-
-        // Test 2: shouldn't be able to add a sample to a completed list
-        instance1.build();
-        gotException = false;
-        try {
-            instance1.addSample(sample);
-        } catch (IllegalStateException e) {
-            gotException = true;
-        }
-        assertTrue(gotException);
-        
-        // Test 3: Should be able to add an in-range sample to an incomplete list with no exception
-        gotException = false;
-        instance2.addSample(sample);
-        assertFalse(gotException);
+        instance1.addSample(-1);
     }
 
     /**
-     * Test of build method, of class PulseListBuilder.
+     * Test of addSample method, of class PulseListBuilder.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddSampleThrowsExceptionForLargeNum() {
+        // Test 1: shouldn't be able to add out of range samples
+        instance1.addSample(256);
+    }
+
+    /**
+     * Test of addSample method, of class PulseListBuilder.
+     */
+    @Test(expected = IllegalStateException.class)
+    public void testAddSampleCantAddSampleToACompletedList() {
+        // Test 2: shouldn't be able to add a sample to a completed list
+        instance1.build();
+        instance1.addSample(0);
+    }
+
+    /**
+     * Test of addSample method, of class PulseListBuilder.
+     */
+    @Test
+    public void testAddSample() {
+        // Test 3: Should be able to add an in-range sample to an incomplete list with no exception
+        instance2.addSample(0);
+    }
+
+    /**
+     * Test of finishPulseList method, of class PulseListBuilder.
      */
     @Test
     public void testFinishPulseList() {
@@ -101,14 +122,12 @@ public class PulseListBuilderTest {
      */
     @Test
     public void testGetPulseList() {
-        System.out.println("getPulseList");
-        
         PulseList pulseList = instance1.build();
         List<Long> result = pulseList.getPulseLengths();
-        
-        assertEquals(238, result.get(0).longValue());
-        assertEquals(238, result.get(1).longValue());
-        assertEquals(79, result.get(2).longValue());
+
+        assertThat(result.get(0), is(238L));
+        assertThat(result.get(1), is(238L));
+        assertThat(result.get(2), is(79L));
     }
 
     /**
@@ -116,53 +135,44 @@ public class PulseListBuilderTest {
      */
     @Test
     public void testFirstPulseLevel() {
-        System.out.println("firstPulseLevel");
-
         // Test 1: Build samples1 should produce first pulse level 0
-        int expResult = 0;
         PulseList pulseList = instance1.build();
-        int result = pulseList.getFirstPulseLevel();
-        assertEquals(expResult, result);
+        assertThat(pulseList.getFirstPulseLevel(), is(0));
         
         // Test 2: Build samples2 should produce first pulse level 1
-        expResult = 1;
         pulseList = instance2.build();
-        result = pulseList.getFirstPulseLevel();
-        assertEquals(expResult, result);
+        assertThat(pulseList.getFirstPulseLevel(), is(1));
     }
-    
+
+    /**
+     * Test of firstPulseLevel method, of class PulseListBuilder.
+     */
+    @Test(expected = IllegalStateException.class)
+    public void testBuildThrowsException() {
+        // Test 0: check we get an exception when built with no pulses added
+        PulseListBuilder instance = new PulseListBuilder(SAMPLE_RATE, MACHINE_HZ);
+        instance.build();
+    }
+
     /**
      * Test of firstPulseLevel method, of class PulseListBuilder.
      */
     @Test
     public void testBuild() {
-        System.out.println("build");
-        
-        // Test 0: check we get an exception when built with no pulses added
-        boolean expResultBool = true;
-        boolean resultBool = false;
         PulseListBuilder instance = new PulseListBuilder(SAMPLE_RATE, MACHINE_HZ);
-        try {
-            instance.build();
-            fail("No exception received");
-        } catch (IllegalStateException e) {
-            resultBool = true;
-        }
-        assertEquals(expResultBool, resultBool);
-        
-        
+
         // Test that builder transitions to complete state when finished
-        assertFalse(instance1.isTapeComplete());
+        assertThat(instance.isTapeComplete(), is(false));
 
         // Test 1: Check we get a PulseList when build() is called and some samples have been received
         PulseList pulseList = instance1.build();
-        assertNotNull(pulseList);
+        assertThat(pulseList, is(notNullValue()));
 
-        assertTrue(instance1.isTapeComplete());
+        assertThat(instance1.isTapeComplete(), is(true));
         
         // Validate that final pulse is closed by this method?
         pulseList = instance2.build();
-        assertEquals(3, pulseList.getPulseLengths().size());
+        assertThat(pulseList.getPulseLengths().size(), is(3));
     }
     
     /**
@@ -170,7 +180,7 @@ public class PulseListBuilderTest {
      */
     @Test
     public void testGetTStatesPerSample() {
-        assertEquals(79.365, instance1.getTStatesPerSample(), TOLERANCE);
+        assertThat(instance1.getTStatesPerSample(), is(closeTo(79.365, TOLERANCE)));
     }
 
 }
