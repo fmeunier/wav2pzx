2e32fe7398482dbe4dfb6e8b1d9acb06d5b651d8
Fredrick Meunier <fredm@spamcop.net>
2016-06-16 18:43:23 +1000

    Add LoaderState state machine and associated tests.

---
+++ src/main/java/xyz/meunier/wav2pzx/blockfinder/LoaderState.java
@@ -0,0 +1,208 @@
+/*
+ * Copyright (c) 2016, Fredrick Meunier
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package xyz.meunier.wav2pzx.blockfinder;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static xyz.meunier.wav2pzx.blockfinder.LoaderContext.*;
+
+/**
+ * State machine that drives the processing of a sequence of pulses, recognising
+ * pilot and data blocks.
+ * <p>
+ * State is all extrinsic and stored in a supplied LoaderContext object.
+ * @author Fredrick Meunier
+ */
+enum LoaderState {
+
+    /**
+     * This is the initial state of the loader, searching for a PILOT tone.
+     * Generates a pulse sequence block.
+     */
+    INITIAL {
+        @Override
+        public LoaderState nextState(LoaderContext context) {
+            if(isaPilotCandidate(context.getCurrentPulse())) {
+                // Close current pulse block - note that this may be empty!
+                context.completeUnknownPulseBlock();
+
+                // Put pulse into new pulse block and pilot pulse stats
+                context.addPilotPulse(context.getCurrentPulse());
+                
+                logTransition(context.getCurrentPulse(), INITIAL, FIND_PILOT);
+                return FIND_PILOT;
+            } else {
+                // Put pulse into current block
+                context.addUnclassifiedPulse(context.getCurrentPulse());
+
+                return INITIAL;
+            }
+        }
+
+        @Override
+        public void endLoader(LoaderContext context) {
+            // Close current pulse block
+            context.completeUnknownPulseBlock();
+        }
+    },
+
+    /**
+     * The parser is in this state if it is currently accumulating pilot pulses.
+     */
+    FIND_PILOT {
+        @Override
+        public LoaderState nextState(LoaderContext context) {
+            if( !isaPilotCandidate(context.getCurrentPulse()) ) {
+                // Not really a new pulse sequence, dump pulses into existing block
+                context.revertCurrentBlock();
+                context.addUnclassifiedPulse(context.getCurrentPulse());
+                
+                logTransition(context.getCurrentPulse(), FIND_PILOT, INITIAL);
+                return INITIAL;
+            }
+            
+            context.addPilotPulse( context.getCurrentPulse() );
+
+            if( context.getNumPilotPulses() >= MIN_PILOT_COUNT ) {
+                logTransition(context.getCurrentPulse(), FIND_PILOT, FIND_PILOT_END);
+                return FIND_PILOT_END;
+            }
+            
+            return FIND_PILOT;
+        }
+
+        @Override
+        public void endLoader(LoaderContext context) {
+            // Close current pulse block
+            context.completeUnknownPulseBlock();
+        }
+    },
+    
+    /**
+     * Look for the end of a sequence of PILOT pulses, when found will either have pulses shorter than MIN_PILOT_LENGTH
+     * for data so transition to GET_DATA or if greater than MAX_PILOT_LENGTH transition back to looking for PILOT
+     * pulses.
+     */
+    FIND_PILOT_END {
+        @Override
+        public LoaderState nextState(LoaderContext context) {
+            if( isaDataCandidate(context.getCurrentPulse()) ) {
+                context.completePilotPulseBlock();
+
+                // Found a candidate sync/data pulse, and re-process this pulse in the new state
+                logTransition(context.getCurrentPulse(), FIND_PILOT_END, GET_DATA);
+                return GET_DATA.nextState(context);
+            } else if( context.getCurrentPulse() > MAX_PILOT_LENGTH ) {
+                context.completePilotPulseBlock();
+
+                // go back to looking for pilot tones, but re-process this pulse in the new state
+                logTransition(context.getCurrentPulse(), FIND_PILOT_END, INITIAL);
+                return INITIAL.nextState(context);
+            }
+
+            context.addPilotPulse( context.getCurrentPulse() );
+            
+            return FIND_PILOT_END;
+        }
+
+        @Override
+        public void endLoader(LoaderContext context) {
+            // Close current pulse block
+            context.completePilotPulseBlock();
+        }
+    },
+
+    /**
+     * Get the data for a data block and the tail pulse if found. When we can't
+     * find any more data bits or a tail pulse, complete the block and head back
+     * to looking for PILOT pulses.
+     */
+    GET_DATA {
+        @Override
+        public LoaderState nextState(LoaderContext context) {
+            // Any two adjacent data pulses must be less than DATA_TOTAL_MAX
+            long nextPulse = context.hasNextPulse() ? context.peekNextPulse() : 0L;
+            if( (context.getCurrentPulse() + nextPulse) > DATA_TOTAL_MAX ) {
+                return handleOptionalTailPulse(context);
+            }
+
+            // Put pulse into current block
+            context.addUnclassifiedPulse(context.getCurrentPulse());
+
+            return GET_DATA;
+        }
+
+        @Override
+        public void endLoader(LoaderContext context) {
+            // Close current data block - TODO: Should check there is a significant size to the block?
+            context.completeDataBlock();
+        }
+
+        private LoaderState handleOptionalTailPulse(LoaderContext context) {
+            final boolean wasTailPulse = context.getCurrentPulse() <= LoaderContext.MAX_TAIL_PULSE;
+
+            if( wasTailPulse ) {
+                context.addUnclassifiedPulse(context.getCurrentPulse());
+            }
+
+            context.completeDataBlock();
+
+            // go back to looking for pilot tones, but re-process this pulse in the new state if we haven't allocated it
+            // to this block
+            logTransition(context.getCurrentPulse(), GET_DATA, INITIAL);
+            return wasTailPulse ? INITIAL : INITIAL.nextState(context);
+        }
+
+    };
+
+    /**
+     * Returns the next state of the state machine depending on the current 
+     * state and the next pulse from the file being analysed.
+     *
+     * @param context the extrinsic state of the Loader
+     * @return the LoaderState
+     */
+    public abstract LoaderState nextState(LoaderContext context);
+    
+    /**
+     * Finalises the current loader as there are no more pulses, flushes any remaining
+     * block data to the context.
+     * @param context the extrinsic state of the Loader
+     */
+    public abstract void endLoader(LoaderContext context);
+    
+    /**
+     * Debug message to track state transitions.
+     * @param currentPulse the pulse that triggered the transition
+     * @param fromState the initial state
+     * @param toState the terminal state
+     */
+    private static void logTransition(Long currentPulse, LoaderState fromState, LoaderState toState) {
+        String message = String.format("%12s -> %12s: %d", fromState, toState, currentPulse);
+        Logger.getLogger(LoaderState.class.getName()).log(Level.FINE, message);
+    }
+}
---
+++ src/test/java/xyz/meunier/wav2pzx/blockfinder/LoaderStateTest.java
@@ -0,0 +1,212 @@
+/*
+ * Copyright (c) 2016, Fredrick Meunier
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package xyz.meunier.wav2pzx.blockfinder;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InOrder;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
+import static xyz.meunier.wav2pzx.blockfinder.LoaderState.*;
+
+public class LoaderStateTest {
+
+    private LoaderContext context;
+
+    @Before
+    public void setUp() throws Exception {
+        context = mock(LoaderContext.class);
+    }
+
+    /**
+     * Test of values method, of class LoaderState.
+     */
+    @Test
+    public void testValues() {
+        assertThat(LoaderState.values(), is(equalTo(new LoaderState[]{INITIAL, FIND_PILOT, FIND_PILOT_END, GET_DATA})));
+    }
+
+    /**
+     * Test of valueOf method, of class LoaderState.
+     */
+    @Test
+    public void testValueOf() {
+        assertThat(INITIAL, is(LoaderState.valueOf("INITIAL")));
+        assertThat(FIND_PILOT, is(LoaderState.valueOf("FIND_PILOT")));
+        assertThat(FIND_PILOT_END, is(LoaderState.valueOf("FIND_PILOT_END")));
+        assertThat(GET_DATA, is(LoaderState.valueOf("GET_DATA")));
+    }
+
+    @Test
+    public void testNextState_From_INITIAL_To_FIND_PILOT() {
+        when(context.getCurrentPulse()).thenReturn(2100L);
+
+        assertThat(INITIAL.nextState(context), is(FIND_PILOT));
+
+        InOrder inOrder = inOrder(context);
+        inOrder.verify(context).completeUnknownPulseBlock();
+        inOrder.verify(context).addPilotPulse(2100L);
+    }
+
+    @Test
+    public void testNextState_From_INITIAL_To_INITIAL() {
+        when(context.getCurrentPulse()).thenReturn(3500L);
+
+        assertThat(INITIAL.nextState(context), is(INITIAL));
+
+        verify(context).addUnclassifiedPulse(3500L);
+        verify(context,never()).completeUnknownPulseBlock();
+    }
+
+    @Test
+    public void testNextState_From_FIND_PILOT_To_INITIAL() {
+        when(context.getCurrentPulse()).thenReturn(3500L);
+
+        assertThat(FIND_PILOT.nextState(context), is(INITIAL));
+
+        InOrder inOrder = inOrder(context);
+        inOrder.verify(context).revertCurrentBlock();
+        inOrder.verify(context).addUnclassifiedPulse(3500L);
+    }
+
+    @Test
+    public void testNextState_From_FIND_PILOT_To_FIND_PILOT_END() {
+        when(context.getCurrentPulse()).thenReturn(2100L);
+        when(context.getNumPilotPulses()).thenReturn(32);
+
+        assertThat(FIND_PILOT.nextState(context), is(FIND_PILOT_END));
+
+        verify(context).addPilotPulse(2100L);
+    }
+
+    @Test
+    public void testNextState_From_FIND_PILOT_To_FIND_PILOT() {
+        when(context.getCurrentPulse()).thenReturn(2100L);
+        when(context.getNumPilotPulses()).thenReturn(5);
+
+        assertThat(FIND_PILOT.nextState(context), is(FIND_PILOT));
+
+        verify(context).addPilotPulse(2100L);
+    }
+
+    @Test
+    public void testNextState_From_FIND_PILOT_END_To_INITIAL() {
+        when(context.getCurrentPulse()).thenReturn(3500L);
+
+        assertThat(FIND_PILOT_END.nextState(context), is(INITIAL));
+
+        InOrder inOrder = inOrder(context);
+        inOrder.verify(context).completePilotPulseBlock();
+        inOrder.verify(context).addUnclassifiedPulse(3500L);
+    }
+
+    @Test
+    public void testNextState_From_FIND_PILOT_END_To_FIND_PILOT_END() {
+        when(context.getCurrentPulse()).thenReturn(2100L);
+
+        assertThat(FIND_PILOT_END.nextState(context), is(FIND_PILOT_END));
+
+        verify(context).addPilotPulse(2100L);
+    }
+
+    @Test
+    public void testNextState_From_GET_DATA_To_GET_DATA() {
+        when(context.getCurrentPulse()).thenReturn(1100L);
+
+        assertThat(GET_DATA.nextState(context), is(GET_DATA));
+
+        verify(context).addUnclassifiedPulse(1100L);
+    }
+
+    @Test
+    public void testNextState_From_GET_DATA_To_INITIAL_lastPulseLong() {
+        // Last pulse in source is a single pulse that is too long to be a tail pulse
+        when(context.getCurrentPulse()).thenReturn(4000L);
+
+        assertThat(GET_DATA.nextState(context), is(INITIAL));
+
+        InOrder inOrder = inOrder(context);
+        inOrder.verify(context).completeDataBlock();
+        inOrder.verify(context).addUnclassifiedPulse(4000L);
+    }
+
+    @Test
+    public void testNextState_From_GET_DATA_To_INITIAL_currentPulseTail() {
+        // Current pulse is a good size to be a tail pulse but following is too long to be a data pulse
+        when(context.getCurrentPulse()).thenReturn(1000L);
+        when(context.hasNextPulse()).thenReturn(true);
+        when(context.peekNextPulse()).thenReturn(3000L);
+
+        assertThat(GET_DATA.nextState(context), is(INITIAL));
+
+        InOrder inOrder = inOrder(context);
+        inOrder.verify(context).addUnclassifiedPulse(1000L);
+        inOrder.verify(context).completeDataBlock();
+    }
+
+    @Test
+    public void testNextState_From_GET_DATA_To_INITIAL_currentPulseNotTail() {
+        // Current pulse is a good size to be a tail pulse but following is too long to be a data pulse
+        when(context.getCurrentPulse()).thenReturn(3000L);
+        when(context.hasNextPulse()).thenReturn(true);
+        when(context.peekNextPulse()).thenReturn(1000L);
+
+        assertThat(GET_DATA.nextState(context), is(INITIAL));
+
+        InOrder inOrder = inOrder(context);
+        inOrder.verify(context).completeDataBlock();
+        inOrder.verify(context).addUnclassifiedPulse(3000L);
+    }
+
+    @Test
+    public void testEndLoader_INITIAL() {
+        INITIAL.endLoader(context);
+        verify(context).completeUnknownPulseBlock();
+    }
+
+    @Test
+    public void testEndLoader_FIND_PILOT() {
+        FIND_PILOT.endLoader(context);
+        verify(context).completeUnknownPulseBlock();
+    }
+
+    @Test
+    public void testEndLoader_FIND_PILOT_END() {
+        FIND_PILOT_END.endLoader(context);
+        verify(context).completePilotPulseBlock();
+    }
+
+    @Test
+    public void testEndLoader_GET_DATA() {
+        GET_DATA.endLoader(context);
+        verify(context).completeDataBlock();
+    }
+
+}
